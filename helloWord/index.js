46b07f45564da4f4247bc0e82ab49c7fee187344
phubao1999 <phutuongbao1999@gmail.com>
2019-06-19 20:47:15 +0700

    Update POST method

--- helloWord/index.js
+++ helloWord/index.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var users = [
     {id: 1, name: 'Bao'},
     {id: 2, name: 'Hien Luong'}
 ];
 
-var port = 3000;
+var port = 1000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', function(req, res){
     res.render('index', {
@@ -32,6 +36,15 @@ app.get('/users/search', function(req, res){
         users: matchedUsers
     });
 });
+
+app.get('/users/create', function(req, res){
+    res.render('users/create');
+});
+
+app.post('/users/create', function(req, res){
+    users.push(req.body);
+    res.redirect('/users');
+})
 
 app.listen(port, function(){
     console.log('Serve Is Listening On Port' + port);
