a8d72d3eced7df7b1c7d17af6566866a64994dff
baophu <baophu@rionlab.com>
2019-11-22 23:35:41 +0700

    Learn Pug, Render

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/", (req, res) =>
+  res.render("./index", {
+    name: "Bao Tuong"
+  })
+);
+
+app.get("/users", (req, res) =>
+  res.render("./users/index", {
+    users: [
+      { id: 1, name: "Bao" },
+      { id: 2, name: "Nhat Linh" },
+      { id: 3, name: "Quang Hung" }
+    ]
+  })
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
