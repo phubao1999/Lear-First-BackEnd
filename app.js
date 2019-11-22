39daeb2f796fe1eb54a1911f8c414f65f40120fc
baophu <baophu@rionlab.com>
2019-11-22 23:56:00 +0700

    Query Params Search

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const port = 3000;
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+const users = [
+  { id: 1, name: "Bao" },
+  { id: 2, name: "Nhat Linh" },
+  { id: 3, name: "Quang Hung" }
+];
+
 app.get("/", (req, res) =>
   res.render("./index", {
     name: "Bao Tuong"
@@ -13,12 +19,18 @@ app.get("/", (req, res) =>
 
 app.get("/users", (req, res) =>
   res.render("./users/index", {
-    users: [
-      { id: 1, name: "Bao" },
-      { id: 2, name: "Nhat Linh" },
-      { id: 3, name: "Quang Hung" }
-    ]
+    users: users
   })
 );
+
+app.get("/users/search", (req, res) => {
+  var q = req.query.q;
+  var machedUsers = users.filter(user => {
+    return user.name.toUpperCase().indexOf(q.toUpperCase()) !== -1;
+  });
+  res.render('users/index', {
+    users: machedUsers  
+  })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
