df2ec9e8e4387591c9baf261eb74106262477435
baophu <baophu@rionlab.com>
2019-11-23 00:23:31 +0700

    Post By Request.body

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -24,7 +28,7 @@ app.get("/users", (req, res) =>
 );
 
 app.get("/users/search", (req, res) => {
-//   console.log(req);
+  //   console.log(req);
   var q = req.query.q;
   var machedUsers = users.filter(user => {
     return user.name.toUpperCase().indexOf(q.toUpperCase()) !== -1;
@@ -32,6 +36,16 @@ app.get("/users/search", (req, res) => {
   res.render("users/index", {
     users: machedUsers
   });
+});
+
+app.get("/users/create", (req, res) => {
+  res.render("./users/create");
+});
+
+app.post("/users/create", (req, res) => {
+//   console.log(req.body);
+    users.push(req.body);
+    res.redirect("/users");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
